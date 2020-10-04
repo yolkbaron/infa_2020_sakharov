9bb6c8218759415730848236744468d4610d990c
Sakharov Daniil <dansacharoff@gmail.com>
2020-10-04 14:40:13 +0300

    added quality parameter to improve surfaces quality

--- lab3/03_draw12upg.py
+++ lab3/03_draw12upg.py
@@ -5,7 +5,7 @@ from pygame.transform import *
 
 def placeflower(surface, pos, angle=0, zoom=1.0):
     sflower = rotozoom(flower, angle, zoom)
-    sflower.set_colorkey((113, 200, 55))
+    sflower.set_colorkey((0, 0, 0))
     surface.blit(sflower, pos)
 
 
@@ -21,70 +21,78 @@ def placekozjol(surface, pos, zoom=1.0, mirror=False):
     surface.blit(skozjol, pos)
 
 
+quality = 10
+
 pygame.init()
 
-flower = pygame.Surface((70, 35), pygame.SRCALPHA)
-flower.fill((113, 200, 55))
-flower.set_colorkey((0, 0, 0))
-ellipse(flower, (255, 255, 255), (17, 0, 25, 13))
-ellipse(flower, (155, 162, 150), (17, 0, 25, 13), 1)
-ellipse(flower, (255, 255, 255), (5, 4, 25, 13))
-ellipse(flower, (155, 162, 150), (5, 4, 25, 13), 1)
-ellipse(flower, (255, 255, 255), (32, 3, 25, 13))
-ellipse(flower, (155, 162, 150), (32, 3, 25, 13), 1)
-ellipse(flower, (255, 255, 0), (21, 11, 25, 13))
-ellipse(flower, (255, 255, 255), (0, 13, 25, 13))
-ellipse(flower, (155, 162, 150), (0, 13, 25, 13), 1)
-ellipse(flower, (255, 255, 255), (39, 9, 25, 13))
-ellipse(flower, (155, 162, 150), (39, 9, 25, 13), 1)
-ellipse(flower, (255, 255, 255), (13, 17, 25, 13))
-ellipse(flower, (155, 162, 150), (13, 17, 25, 13), 1)
-ellipse(flower, (255, 255, 255), (32, 18, 25, 13))
-ellipse(flower, (155, 162, 150), (32, 18, 25, 13), 1)
+flower = pygame.Surface((70 * quality, 35 * quality))
+flower.fill((0, 0, 0))
+ellipse(flower, (255, 255, 255), (17 * quality, 0 * quality, 25 * quality, 13 * quality))
+ellipse(flower, (155, 162, 150), (17 * quality, 0 * quality, 25 * quality, 13 * quality), 1 * quality)
+ellipse(flower, (255, 255, 255), (5 * quality, 4 * quality, 25 * quality, 13 * quality))
+ellipse(flower, (155, 162, 150), (5 * quality, 4 * quality, 25 * quality, 13 * quality), 1 * quality)
+ellipse(flower, (255, 255, 255), (32 * quality, 3 * quality, 25 * quality, 13 * quality))
+ellipse(flower, (155, 162, 150), (32 * quality, 3 * quality, 25 * quality, 13 * quality), 1 * quality)
+ellipse(flower, (255, 255, 0), (21 * quality, 11 * quality, 25 * quality, 13 * quality))
+ellipse(flower, (255, 255, 255), (0 * quality, 13 * quality, 25 * quality, 13 * quality))
+ellipse(flower, (155, 162, 150), (0 * quality, 13 * quality, 25 * quality, 13 * quality), 1 * quality)
+ellipse(flower, (255, 255, 255), (39 * quality, 9 * quality, 25 * quality, 13 * quality))
+ellipse(flower, (155, 162, 150), (39 * quality, 9 * quality, 25 * quality, 13 * quality), 1 * quality)
+ellipse(flower, (255, 255, 255), (13 * quality, 17 * quality, 25 * quality, 13 * quality))
+ellipse(flower, (155, 162, 150), (13 * quality, 17 * quality, 25 * quality, 13 * quality), 1 * quality)
+ellipse(flower, (255, 255, 255), (32 * quality, 18 * quality, 25 * quality, 13 * quality))
+ellipse(flower, (155, 162, 150), (32 * quality, 18 * quality, 25 * quality, 13 * quality), 1 * quality)
 
-bush = pygame.Surface((300, 300))
+bush = pygame.Surface((300 * quality, 300 * quality))
 bush.fill((170, 222, 135))
-circle(bush, (113, 200, 55), (150, 150), 150)
-placeflower(bush, (132, 25), angle=0, zoom=1.0)
-placeflower(bush, (52, 49), angle=35, zoom=0.75)
-placeflower(bush, (66, 108), angle=15, zoom=0.8)
-placeflower(bush, (189, 73), angle=-30, zoom=1.5)
-placeflower(bush, (90, 165), angle=-15, zoom=2.0)
-placeflower(bush, (227, 150), angle=-70, zoom=1.0)
+circle(bush, (113, 200, 55), (150 * quality, 150 * quality), 150 * quality)
+placeflower(bush, (132 * quality, 25 * quality), angle=0, zoom=1.0)
+placeflower(bush, (52 * quality, 49 * quality), angle=35, zoom=0.75)
+placeflower(bush, (66 * quality, 108 * quality), angle=15, zoom=0.8)
+placeflower(bush, (189 * quality, 73 * quality), angle=-30, zoom=1.5)
+placeflower(bush, (90 * quality, 165 * quality), angle=-15, zoom=2.0)
+placeflower(bush, (227 * quality, 150 * quality), angle=-70, zoom=1.0)
 
-kozjol = pygame.Surface((230, 350))
+kozjol = pygame.Surface((230 * quality, 350 * quality))
 kozjol.fill((170, 222, 135))
 # body
-ellipse(kozjol, (255, 255, 255), (147, 17, 66, 40))
-ellipse(kozjol, (255, 255, 255), (140, 50, 49, 134))
-ellipse(kozjol, (255, 255, 255), (0, 153, 182, 76))
-ellipse(kozjol, (255, 255, 255), (11, 191, 25, 48))
-ellipse(kozjol, (255, 255, 255), (10, 234, 28, 56))
-ellipse(kozjol, (255, 255, 255), (16, 288, 28, 18))
-ellipse(kozjol, (255, 255, 255), (46, 212, 27, 60))
-ellipse(kozjol, (255, 255, 255), (45, 269, 29, 55))
-ellipse(kozjol, (255, 255, 255), (52, 322, 28, 18))
-ellipse(kozjol, (255, 255, 255), (114, 182, 25, 52))
-ellipse(kozjol, (255, 255, 255), (113, 230, 26, 55))
-ellipse(kozjol, (255, 255, 255), (120, 283, 26, 19))
-ellipse(kozjol, (255, 255, 255), (137, 208, 28, 64))
-ellipse(kozjol, (255, 255, 255), (138, 267, 27, 56))
-ellipse(kozjol, (255, 255, 255), (144, 321, 27, 19))
-ellipse(kozjol, (255, 255, 255), (144, 321, 27, 19))
+ellipse(kozjol, (255, 255, 255), (147 * quality, 17 * quality, 66 * quality, 40 * quality))
+ellipse(kozjol, (255, 255, 255), (140 * quality, 50 * quality, 49 * quality, 134 * quality))
+ellipse(kozjol, (255, 255, 255), (0 * quality, 153 * quality, 182 * quality, 76 * quality))
+ellipse(kozjol, (255, 255, 255), (11 * quality, 191 * quality, 25 * quality, 48 * quality))
+ellipse(kozjol, (255, 255, 255), (10 * quality, 234 * quality, 28 * quality, 56 * quality))
+ellipse(kozjol, (255, 255, 255), (16 * quality, 288 * quality, 28 * quality, 18 * quality))
+ellipse(kozjol, (255, 255, 255), (46 * quality, 212 * quality, 27 * quality, 60 * quality))
+ellipse(kozjol, (255, 255, 255), (45 * quality, 269 * quality, 29 * quality, 55 * quality))
+ellipse(kozjol, (255, 255, 255), (52 * quality, 322 * quality, 28 * quality, 18 * quality))
+ellipse(kozjol, (255, 255, 255), (114 * quality, 182 * quality, 25 * quality, 52 * quality))
+ellipse(kozjol, (255, 255, 255), (113 * quality, 230 * quality, 26 * quality, 55 * quality))
+ellipse(kozjol, (255, 255, 255), (120 * quality, 283 * quality, 26 * quality, 19 * quality))
+ellipse(kozjol, (255, 255, 255), (137 * quality, 208 * quality, 28 * quality, 64 * quality))
+ellipse(kozjol, (255, 255, 255), (138 * quality, 267 * quality, 27 * quality, 56 * quality))
+ellipse(kozjol, (255, 255, 255), (144 * quality, 321 * quality, 27 * quality, 19 * quality))
+ellipse(kozjol, (255, 255, 255), (144 * quality, 321 * quality, 27 * quality, 19 * quality))
 # eye
-ellipse(kozjol, (229, 128, 255), (161, 20, 31, 27))
-ellipse(kozjol, (0, 0, 0), (177, 27, 12, 11))
-flare = pygame.Surface((15, 8))
+ellipse(kozjol, (229, 128, 255), (161 * quality, 20 * quality, 31 * quality, 27 * quality))
+ellipse(kozjol, (0, 0, 0), (177 * quality, 27 * quality, 12 * quality, 11 * quality))
+flare = pygame.Surface((15 * quality, 8 * quality))
 flare.fill((229, 128, 255))
 flare.set_colorkey((229, 128, 255))
-ellipse(flare, (255, 255, 255), (0, 0, 15, 8))
+ellipse(flare, (255, 255, 255), (0 * quality, 0 * quality, 15 * quality, 8 * quality))
 flare = rotate(flare, -30)
-kozjol.blit(flare, (166, 19))
+kozjol.blit(flare, (166 * quality, 19 * quality))
 # horns(ears?)
-polygon(kozjol, (255, 255, 255), [(155, 26), (148, 24), (139, 17), (132, 6), (132, 2), (132, 8), (135, 18), (142, 27),
-                                  (148, 34), (151, 34)])
-polygon(kozjol, (255, 255, 255), [(164, 21), (157, 18), (152, 14), (149, 9), (146, 6), (145, 0), (145, 5), (146, 9),
-                                  (147, 12), (149, 15), (150, 17), (152, 20), (154, 22), (156, 25)])
+polygon(kozjol, (255, 255, 255),
+        [(155 * quality, 26 * quality), (148 * quality, 24 * quality), (139 * quality, 17 * quality),
+         (132 * quality, 6 * quality), (132 * quality, 2 * quality), (132 * quality, 8 * quality),
+         (135 * quality, 18 * quality), (142 * quality, 27 * quality), (148 * quality, 34 * quality),
+         (151 * quality, 34 * quality)])
+polygon(kozjol, (255, 255, 255),
+        [(164 * quality, 21 * quality), (157 * quality, 18 * quality), (152 * quality, 14 * quality),
+         (149 * quality, 9 * quality), (146 * quality, 6 * quality), (145 * quality, 0 * quality),
+         (145 * quality, 5 * quality), (146 * quality, 9 * quality), (147 * quality, 12 * quality),
+         (149 * quality, 15 * quality), (150 * quality, 17 * quality), (152 * quality, 20 * quality),
+         (154 * quality, 22 * quality), (156 * quality, 25 * quality)])
 
 FPS = 30
 screen = pygame.display.set_mode(size=(800, 1130))
@@ -108,19 +116,19 @@ polygon(screen, (0, 0, 0), [(0, 347), (95, 115), (165, 276), (272, 148), (473, 4
                             (433, 565), (417, 563), (176, 563),
                             (101, 574), (77, 574), (43, 579), (0, 595)], 2)
 # bushes
-placebush(screen, (-10, 578), 0.23, mirror=False)
-placebush(screen, (563, 667), 0.23, mirror=False)
-placebush(screen, (516, 898), 0.6, mirror=False)
-placebush(screen, (690, 607), 0.36, mirror=True)
-placebush(screen, (679, 734), 0.6, mirror=True)
-placebush(screen, (760, 1030), 0.32, mirror=True)
+placebush(screen, (-10, 578), 0.23/quality, mirror=False)
+placebush(screen, (563, 667), 0.23/quality, mirror=False)
+placebush(screen, (516, 898), 0.6/quality, mirror=False)
+placebush(screen, (690, 607), 0.36/quality, mirror=True)
+placebush(screen, (679, 734), 0.6/quality, mirror=True)
+placebush(screen, (760, 1030), 0.32/quality, mirror=True)
 # animals
-placekozjol(screen, (283, 465), 0.4, mirror=False)
-placekozjol(screen, (172, 583), 0.4, mirror=False)
-placekozjol(screen, (325, 639), 0.4, mirror=True)
+placekozjol(screen, (283, 465), 0.4/quality, mirror=False)
+placekozjol(screen, (172, 583), 0.4/quality, mirror=False)
+placekozjol(screen, (325, 639), 0.4/quality, mirror=True)
 
-placekozjol(screen, (665, 593), 1.2, mirror=True)
-placekozjol(screen, (-280, 715), 2.2, mirror=False)
+placekozjol(screen, (665, 593), 1.2/quality, mirror=True)
+placekozjol(screen, (-280, 715), 2.2/quality, mirror=False)
 
 pygame.display.update()
 clock = pygame.time.Clock()
