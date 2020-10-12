fb3043240bd5abe26c501e74f02cb9f377534ff4
Sakharov Daniil <dansacharoff@gmail.com>
2020-10-12 11:55:04 +0300

    static balls disappear after being clicked

--- lab5/game.py
+++ lab5/game.py
@@ -5,7 +5,10 @@ from random import randint
 pygame.init()
 
 FPS = 2
-screen = pygame.display.set_mode((1200, 900))
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 900
+NUMBER_OF_BALLS = 5
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -18,39 +21,90 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
 class Ball:
+    pos = (0, 0)
+    velocity = (0, 0)
+    color = RED
+    radius = 100
+
     def __init__(self, pos, velocity, color, radius):
         self.pos = pos
         self.velocity = velocity
         self.color = color
         self.radius = radius
 
+    def new(self):
+        """
+        generates new ball with random color and radius in random place
+        """
+        new_ball = create_ball()
+        self.pos = new_ball.pos
+        self.velocity = new_ball.velocity
+        self.color = new_ball.color
+        self.radius = new_ball.radius
+        return new_ball
 
-def ball_generator():
+
+def create_ball():
     """
-    draws a new ball with random color and radius in random place
+    creates random new object of Ball type
+    :return: this object
     """
-    global x, y, r
-    for i in range(randint(1, 10)):
-        x = randint(100, 1100)
-        y = randint(100, 900)
-        r = randint(10, 100)
-        color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    radius = randint(10, 100)
+    color = COLORS[randint(0, 5)]
+    x = randint(radius, SCREEN_WIDTH - radius)
+    y = randint(radius, SCREEN_HEIGHT - radius)
+    vx = randint(-1, 1) * SCREEN_WIDTH / randint(5, 10)
+    vy = randint(-1, 1) * SCREEN_HEIGHT / randint(5, 10)
+    pos = (x, y)
+    velocity = (vx, vy)
+    ball = Ball(pos, velocity, color, radius)
+    return ball
 
 
-def click(click_event, click_score):
+def click(balls, click_event, click_score):
     """
     checks if click was inside a ball
+    and removes ball if so
     """
-    if (click_event.pos[0] - x) ** 2 + (click_event.pos[1] - y) ** 2 <= r ** 2:
-        click_score += 1
+    missed = True
+    for i in range(NUMBER_OF_BALLS):
+        ball = balls[i]
+        ball_x = ball.pos[0]
+        ball_y = ball.pos[1]
+        radius = ball.radius
+        mouse_x = click_event.pos[0]
+        mouse_y = click_event.pos[1]
+        if (mouse_x - ball_x) * (mouse_x - ball_x) + (mouse_y - ball_y) * (mouse_y - ball_y) <= radius*radius:
+            click_score += 10
+            balls[i] = create_ball()
+            missed = False
+            print("Scored")
+    if missed:
+        click_score -= 5
     return click_score
+
+
+def balls_move(balls):
+    SCREEN.fill(BLACK)
+    for i in range(NUMBER_OF_BALLS):
+        ball = balls[i]
+        x = ball.pos[0]
+        y = ball.pos[1]
+        vx = ball.velocity[0]
+        vy = ball.velocity[1]
+        color = ball.color
+        pos = (x, y)
+        radius = ball.radius
+        circle(SCREEN, color, pos, radius)
 
 
 score = 0
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
+balls = []
+for i in range(NUMBER_OF_BALLS):
+    balls.append(create_ball())
 
 while not finished:
     clock.tick(FPS)
@@ -59,9 +113,9 @@ while not finished:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print('Click!')
-            score = click(event, score)
-    ball_generator()
+            score = click(balls, event, score)
+    balls_move(balls)
     pygame.display.update()
-    screen.fill(BLACK)
+    SCREEN.fill(BLACK)
 print("Score = ", score)
 pygame.quit()
