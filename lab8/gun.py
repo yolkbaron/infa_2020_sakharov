2e636b0cef944800591d0ec1c1f97db4df788be4
Comedenti <dansacharoff@gmail.com>
2020-11-01 22:56:26 +0300

    Can't be compiled

--- lab8/gun.py
+++ lab8/gun.py
@@ -1,186 +1,177 @@
-from random import randrange as rnd, choice
-import tkinter as tk
+import random as rnd
 import math
-import time
-
-# print (dir(math))
-
-root = tk.Tk()
-fr = tk.Frame(root)
-root.geometry('800x600')
-canv = tk.Canvas(root, bg='white')
-canv.pack(fill=tk.BOTH, expand=1)
+import pygame
 
 
-class Ball:
-    def __init__(self, x=40, y=450):
-        """ Конструктор класса ball
+FPS = 20
+GRAVITY_ACCELERATION = 9.8  # Ускорение свободного падения для снаряда.
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (100, 100, 100)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-        Args:
-        x - начальное положение мяча по горизонтали
-        y - начальное положение мяча по вертикали
-        """
+class Cannon:
+    max_velocity = 10
+
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.r = 10
-        self.vx = 0
-        self.vy = 0
-        self.color = choice(['blue', 'green', 'red', 'brown'])
-        self.id = canv.create_oval(
-            self.x - self.r,
-            self.y - self.r,
-            self.x + self.r,
-            self.y + self.r,
-            fill=self.color
-        )
-        self.live = 30
+        self.shell_num = None  # TODO: оставшееся на данный момент количество снарядов
+        self.direction = math.pi / 4
 
-    def set_coords(self):
-        canv.coords(
-            self.id,
-            self.x - self.r,
-            self.y - self.r,
-            self.x + self.r,
-            self.y + self.r
-        )
-
-    def move(self):
-        """Переместить мяч по прошествии единицы времени.
-
-        Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
-        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
-        и стен по краям окна (размер окна 800х600).
+    def aim(self, x, y):
         """
-        # FIXME
-        self.x += self.vx
-        self.y -= self.vy
-
-    def hittest(self, obj):
-        """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
-
-        Args:
-            obj: Обьект, с которым проверяется столкновение.
-        Returns:
-            Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
+        Меняет направление direction так, чтобы он из точки
+         (self.x, self.y) указывал в точку (x, y).
+        :param x: координата x, в которую целимся
+        :param y: координата y, в которую целимся
+        :return: None
         """
-        # FIXME
-        if True:
-            return False
+        pass  # TODO
 
-
-class Gun:
-    def __init__(self):
-        self.f2_power = 10
-        self.f2_on = 0
-        self.an = 1
-        self.id = canv.create_line(20, 450, 50, 420, width=7)
-
-    def fire2_start(self, event):
-        self.f2_on = 1
-
-    def fire2_end(self, event):
-        """Выстрел мячом.
-
-        Происходит при отпускании кнопки мыши.
-        Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
+    def fire(self, dt):
         """
-        global balls, bullet
-        bullet += 1
-        new_ball = Ball()
-        new_ball.r += 5
-        self.an = math.atan((event.y - new_ball.y) / (event.x - new_ball.x))
-        new_ball.vx = self.f2_power * math.cos(self.an)
-        new_ball.vy = - self.f2_power * math.sin(self.an)
-        balls += [new_ball]
-        self.f2_on = 0
-        self.f2_power = 10
+        Создаёт объект снаряда (если ещё не потрачены все снаряды)
+        летящий в направлении угла direction
+        со скоростью, зависящей от длительности клика мышки
+        :param dt:  длительность клика мышки, мс
+        :return: экземпляр снаряда типа Shell
+        """
+        pass
 
-    def targetting(self, event=0):
-        """Прицеливание. Зависит от положения мыши."""
-        if event:
-            self.an = math.atan((event.y - 450) / (event.x - 20))
-        if self.f2_on:
-            canv.itemconfig(self.id, fill='orange')
-        else:
-            canv.itemconfig(self.id, fill='black')
-        canv.coords(self.id, 20, 450,
-                    20 + max(self.f2_power, 20) * math.cos(self.an),
-                    450 + max(self.f2_power, 20) * math.sin(self.an)
-                    )
+    def draw(self):
+        pygame.draw.circle(screen, self.color,
+                           (int(round(self.x)), int(round(self.y))), self.r)
 
-    def power_up(self):
-        if self.f2_on:
-            if self.f2_power < 100:
-                self.f2_power += 1
-            canv.itemconfig(self.id, fill='orange')
-        else:
-            canv.itemconfig(self.id, fill='black')
+
+class Shell:
+    standard_radius = 25
+
+    def __init__(self, x, y, Vx, Vy):
+        self.x, self.y = x, y
+        self.Vx, self.Vy = Vx, Vy
+        self.r = Shell.standard_radius
+
+    def move(self, dt):
+        """
+        Сдвигает снаряд исходя из его кинематических характеристик
+        и длины кванта времени dt
+        в новое положение, а также меняет его скорость.
+        :param dt:
+        :return:
+        """
+        ax, ay = 0, GRAVITY_ACCELERATION
+        self.x += self.Vx*dt + ax*(dt**2)/2
+        self.y += self.Vy*dt + ay*(dt**2)/2
+        self.Vx += ax*dt
+        self.Vy += ay*dt
+        # TODO: Уничтожать (в статус deleted) снаряд, когда он касается земли.
+
+    def draw(self):
+        pygame.draw.circle(screen, self.color,
+                           (int(round(self.x)), int(round(self.y))), self.r)
+
+    def detect_collision(self, other):
+        """
+        Проверяет факт соприкосновения снаряда и объекта other
+        :param other: объект, который должен иметь поля x, y, r
+        :return: логическое значение типа bool
+        """
+        length = ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+        return length <= self.r + other.r
 
 
 class Target:
-    def __init__(self):
-        self.points = 0
-        self.live = 1
-        self.id = canv.create_oval(0, 0, 0, 0)
-        self.id_points = canv.create_text(30, 30, text=self.points, font='28')
-        self.new_target()
+    standard_radius = 15
 
-    def new_target(self):
-        """ Инициализация новой цели. """
-        x = self.x = rnd(600, 780)
-        y = self.y = rnd(300, 550)
-        r = self.r = rnd(2, 50)
-        color = self.color = 'red'
-        canv.coords(self.id, x - r, y - r, x + r, y + r)
-        canv.itemconfig(self.id, fill=color)
+    def __init__(self, x, y, Vx, Vy):
+        self.x, self.y = x, y
+        self.Vx, self.Vy = Vx, Vy
+        self.r = Target.standard_radius
+        self.color = COLORS[rnd.randint(0, len(COLORS) - 1)]
 
-    def hit(self, points=1):
-        """Попадание шарика в цель."""
-        canv.coords(self.id, -10, -10, -10, -10)
-        self.points += points
-        canv.itemconfig(self.id_points, text=self.points)
+    def move(self, dt):
+        """
+        Сдвигает шарик-мишень исходя из его кинематических характеристик
+        и длины кванта времени dt
+        в новое положение, а также меняет его скорость.
+        :param dt:
+            :return:
+        """
+        ax, ay = 0, GRAVITY_ACCELERATION
+        self.x += self.Vx * dt
+        self.y += self.Vy * dt
+        self.Vx += ax * dt
+        self.Vy += ay * dt
+        # TODO: Шарики-мишени должны отражаться от стенок
 
+    def draw(self):
+        pygame.draw.circle(screen, self.color,
+                           (int(round(self.x)), int(round(self.y))), self.r)
 
-t1 = Target()
-screen1 = canv.create_text(400, 300, text='', font='28')
-g1 = Gun()
-bullet = 0
-balls = []
-
-
-def new_game(event=''):
-    global gun, t1, screen1, balls, bullet
-    t1.new_target()
-    bullet = 0
-    balls = []
-    canv.bind('<Button-1>', g1.fire2_start)
-    canv.bind('<ButtonRelease-1>', g1.fire2_end)
-    canv.bind('<Motion>', g1.targetting)
-
-    z = 0.03
-    t1.live = 1
-    while t1.live or balls:
-        for b in balls:
-            b.move()
-            if b.hittest(t1) and t1.live:
-                t1.live = 0
-                t1.hit()
-                canv.bind('<Button-1>', '')
-                canv.bind('<ButtonRelease-1>', '')
-                canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
-        canv.update()
-        time.sleep(0.03)
-        g1.targetting()
-        g1.power_up()
-    canv.itemconfig(screen1, text='')
-    canv.delete(gun)
-    root.after(750, new_game)
+    def collide(self, other):
+        """
+        Расчёт абсолютно упругого соударения
+        :param other:
+        :return:
+        """
+        pass  #TODO
 
 
-def mainloop():
+class Bomb:
     pass
 
 
-new_game()
+def generate_random_targets(number: int):
+    targets = []
+    for i in range(number):
+        x = rnd.randint(0, SCREEN_HEIGHT)
+        y = rnd.randint(0, SCREEN_HEIGHT)
+        Vx = rnd.randint(-30, +30)
+        Vy = rnd.randint(-30, +30)
+        target = Target(x, y, Vx, Vy)
+        targets.append(target)
+    return targets
 
-mainloop()
+
+def game_main_loop():
+
+    targets = generate_random_targets(10)
+
+    clock = pygame.time.Clock()
+    finished = False
+
+    while not finished:
+        dt = clock.tick(FPS) / 1000
+        print(dt)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print('Click!')
+
+        pygame.display.update()
+        screen.fill(GRAY)
+
+        for target in targets:
+            target.move(dt)
+
+        for target in targets:
+            target.draw()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.update()
+
+    game_main_loop()
